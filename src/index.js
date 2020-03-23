19a9e9d8dfb6613baa719d5b9720bf3d2a34ac70
Lucas Correa <lucas.dcorrea1@gmail.com>
2020-03-23 18:22:18 -0300

    remove css Global

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/css/Global.css'
+// import './styles/css/'
 import 'rsuite/dist/styles/rsuite-dark.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
